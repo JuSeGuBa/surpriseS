beb87133a2d8f674ce405022f2e1adcff3976f26
sebasps532@gmail.com <you@example.comsebasps532@gmail.com>
2025-09-01 17:35:07 -0500

    story

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,11 +1,19 @@
 const Story = () => {
   return (
     <section className="mb-16">
-      <h2 className="text-2xl font-semibold mb-4">Nuestra Historia</h2>
+      <h2 className="text-2xl font-semibold mb-4">Nuestra pequeña Historia</h2>
       <p className="text-gray-700 leading-relaxed">
-        Nos conocimos de una manera única, y desde entonces cada momento a tu
-        lado ha sido un capítulo maravilloso de mi vida. Este sitio es un
-        pequeño regalo para recordarte lo especial que eres para mí.
+        Nos conocimos en 2014, gracias a Briñez, cuando teníamos apenas 6 años.
+        Desde ese momento hubo una conexión especial, marcaste mi infancia y
+        también mi vida, ¡y fuiste mi primer beso! Aunque éramos pequeños, jamás
+        imaginamos que ahí empezaba nuestra historia. Con el tiempo nos
+        alejamos, cada uno tomó su camino, pero en 2023 nos reencontramos.
+        Aunque no estábamos listos, la conexión volvió al vernos y al bailar. En
+        octubre de 2024 retomamos nuestra historia. No ha sido fácil, con la
+        distancia y los desafíos, pero Te Amo y es un privilegio compartir mi
+        vida contigo. Todavía tenemos páginas por escribir, recuerdos por crear
+        y una vida por delante. Gracias por estar conmigo Mi amor, y que siempre
+        te amaré. Estoy emocionado por todo lo que aún nos espera.
       </p>
     </section>
   );
